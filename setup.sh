edfad80bd25b9505b198fa58026d12e5134c26d8
Dahee Goo <heeouo@kookmin.ac.kr>
2022-01-15 11:53:40 +0900

    chore: add sudo authority

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # install httpd (Linux 2 version)
-yum update -y
-yum install -y httpd
-systemctl start httpd
-systemctl enable httpd
+sudo yum install -y httpd
+sudo systemctl start httpd
+sudo systemctl enable httpd
